4aebc7d78d000b141c24f7037b526ef9fbf40810
hungnv_580 <hungnv_580@vnu.edu.vn>
2017-02-04 19:20:22 +0700

    init

---
+++ login/login.js
@@ -0,0 +1,8 @@
+$('.fliper-btn').click(function(){
+    $('.flip').find('.card').toggleClass('flipped');
+});
+
+$("#password_confirm").click(function () {
+    var password = $('#password').val();
+
+});
